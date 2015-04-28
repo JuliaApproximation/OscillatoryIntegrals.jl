fa448f2298044b38fd4ef6e497ab3ae86aae2ee7
Sheehan Olver <solver@mac.com>
2015-04-28 15:06:53 +1000

    Added Weber's method for calculating Fourier integrals

--- src/OscillatoryIntegrals.jl
+++ src/OscillatoryIntegrals.jl
@@ -14,11 +14,39 @@ export fourier,fourierintegral
 fourierintegral(f::Fun{Chebyshev},ω)=ω==0?integrate(f):
         [BasisFunctional(ceil(Integer,ω));Derivative()+im*ω]\[0.,f]
 
-## fourier returns
+## fourier returns the fourier transform
+# \int f(x) exp(i*w*x) dx
 
 function fourier(f,ω)
     u=fourierintegral(f,ω)
     last(u)*exp(im*ω)-first(u)*exp(-im*ω)
+end
+
+
+
+## Webers algorithm for Fourier transforms
+
+function webersum(cfs,ω)
+    p0=0
+    p1=1
+    ret=p1*first(cfs)
+    for k=2:length(cfs)
+        p1,p0=2*(1-k+ω)/(k-1)*p1-p0,p1
+        ret+=p1*cfs[k]
+    end
+    ret
+end
+
+function fourier(f::Fun{Laurent},ω)
+    @assert domain(f)==PeriodicLine()
+
+    if ω==0
+        sum(f)
+    elseif ω>0
+        4π*exp(-ω)*webersum(f.coefficients[2:2:end],ω)
+    else # ω<0
+        4π*exp(ω)*webersum(f.coefficients[3:2:end],-ω)
+    end
 end
 
 end #module
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,3 +8,13 @@ for ω in (0.,0.001,0.1,1.,10.5,100.,1000.5)
    @test norm(diff(fourierintegral(f,ω)*exp(im*ω*x)) -f*exp(im*ω*x)) < 1000eps()
    @test_approx_eq sum(f*exp(im*ω*x)) fourier(f,ω)
 end
+
+
+f=Fun(sech,Laurent(PeriodicLine()))
+@test_approx_eq_eps fourier(f,10.4) 5.051528742182663e-7 10e-13
+@test_approx_eq_eps fourier(f,-10.4) 5.051528742182663e-7 10e-13
+
+f=Fun(x->sech(x-0.1),Laurent(PeriodicLine()))
+@test_approx_eq_eps fourier(f,10.4) (2.557186179286481e-7 + 4.356459741299553e-7*im) 10e-13
+@test_approx_eq_eps fourier(f,-10.4) (2.557186179286481e-7 - 4.356459741299553e-7*im) 10e-13
+
